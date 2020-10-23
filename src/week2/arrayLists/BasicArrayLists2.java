155a84d4112bf034db165b1396d7cca5a022cfcb
heymiriam <miriambellon@gmail.com>
2020-10-23 16:48:55 -0700

    Finish friday exercise 2

--- src/week2/arrayLists/BasicArrayLists2.java
+++ src/week2/arrayLists/BasicArrayLists2.java
@@ -7,10 +7,6 @@ public class BasicArrayLists2 {
   public static void main(String[] args){
     ArrayList<Integer> nums = new ArrayList<>();
 
-
-
-
-
     for(int i =0; i<10; i++){
       Random randoms = new Random();
       int numbers= randoms.nextInt(100) + 1 ;
